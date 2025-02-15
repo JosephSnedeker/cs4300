1efaf1dc14563e847a283101809fff8152c3f3f1
JosephSnedeker <jsnedeke@uccs.edu>
2025-02-15 20:17:41 +0000

    models

--- Homework2/DjangoHW2/bookings/models.py
+++ Homework2/DjangoHW2/bookings/models.py
@@ -2,8 +2,8 @@ Movie:
     Fields: title (CharField), description (TextField), release_date (DateField), duration (IntegerField).
 
 Seat: 
-    Fields: seat_number (CharField), is_booked (BooleanField)
+    Fields: seat_number (CharField), is_booked (BooleanField).
 
 Booking:
-    Fields: movie (ForeignKey to Movie), seat (ForeignKey to Seat), user (ForeignKey to User), booking_date (DateTimeField)
+    Fields: movie (ForeignKey to Movie), seat (ForeignKey to Seat), user (ForeignKey to User), booking_date (DateTimeField).
 
